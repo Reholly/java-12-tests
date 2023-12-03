275e79e78e83498c323f7e4fc21b3d5b449e0841
Reholly <eu.ustiantsev@yandex.ru>
2023-12-03 22:30:12 +0500

    done

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -6,88 +6,93 @@ public class Tests {
     //Тест преобразования положительных чисел.
     @Test
     public void testPositiveCorrectResults() {
-        Assertions.assertThat(Integer.parseInt("123", 10))
+        Assertions.assertThat(Integer.decode("+123"))
                 .isEqualTo(123);
     }
 
     //Тест преобразования отрициательных чисел.
     @Test
     public void testNegativeCorrectResults() {
-        //Проверка на успешный parse положительных чисел
-        Assertions.assertThat(Integer.parseInt("123", 10))
-                .isEqualTo(123);
+        Assertions.assertThat(Integer.decode("-123"))
+                .isEqualTo(-123);
     }
 
     //Тест преобразования нулевых чисел.
     @Test
     public void testZeroCorrectResults() {
         //Проверка на успешный parse положительных чисел
-        Assertions.assertThat(Integer.parseInt("0", 10))
+        Assertions.assertThat(Integer.decode("0"))
                 .isEqualTo(0);
     }
 
     //Тест преобразования нулевых чисел.
     @Test
     public void testWordStringParsing() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("this is just a string", 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode("this is just a string"))
                 .isInstanceOf(NumberFormatException.class);
     }
 
     //Тест выхода за пределы СС
     @Test
-    public void testCountingSystems() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("35", 2))
+    public void testFloatDecoding() {
+        Assertions.assertThatThrownBy(() -> Integer.decode("35.2"))
                 .isInstanceOf(NumberFormatException.class);
     }
 
     //Тест на выход за границы
     @Test
     public void testNumberParsingBorders() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt(Long.toString((long) Integer.MAX_VALUE + 1), 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode(Long.toString((long) Integer.MAX_VALUE + 1)))
                 .isInstanceOf(NumberFormatException.class);
 
-        Assertions.assertThatThrownBy(() -> Integer.parseInt(Long.toString((long) Integer.MIN_VALUE - 1), 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode(Long.toString((long) Integer.MIN_VALUE - 1)))
                 .isInstanceOf(NumberFormatException.class);
     }
 
     // Тест на корректность написания числа
     @Test
     public void testNumberCorrectRecord() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt(",", 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode(","))
                 .isInstanceOf(NumberFormatException.class);
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("=", 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode("="))
                 .isInstanceOf(NumberFormatException.class);
-        Assertions.assertThatThrownBy(() -> Integer.parseInt(".", 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode("."))
                 .isInstanceOf(NumberFormatException.class);
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("-", 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode("-"))
                 .isInstanceOf(NumberFormatException.class);
     }
 
     // Тест на преобразование null
     @Test
     public void testNullStringParsing() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt(null, 10))
-                .isInstanceOf(NumberFormatException.class);
+        Assertions.assertThatThrownBy(() -> Integer.decode(null))
+                .isInstanceOf(NullPointerException.class);
     }
 
     // Тест на преобразование пустой строки
     @Test
     public void testEmptyStringParsing() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("", 10))
+        Assertions.assertThatThrownBy(() -> Integer.decode(""))
                 .isInstanceOf(NumberFormatException.class);
     }
 
-    // Тест на преобразование с минимальной системой счисления: 2
+    //тест на нулевую строку подстроку сначала
     @Test
-    public void testNumberMinimumRadix() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("", 1))
-                .isInstanceOf(NumberFormatException.class);
+    public void testZeroStart() {
+        Assertions.assertThat(Integer.decode("00001"))
+                .isEqualTo(1);
     }
 
-    // Тест на преобразование с минимальной системой счисления: 36
     @Test
-    public void testNumberMaximumRadix() {
-        Assertions.assertThatThrownBy(() -> Integer.parseInt("", 36))
-                .isInstanceOf(NumberFormatException.class);
+    public void testHexRadix() {
+        Assertions.assertThat(Integer.decode("#A"))
+                .isEqualTo(10);
+        Assertions.assertThat(Integer.decode("#C"))
+                .isEqualTo(12);
+
+        Assertions.assertThat(Integer.decode("-#A"))
+                .isEqualTo(-10);
+        Assertions.assertThat(Integer.decode("-#C"))
+                .isEqualTo(-12);
     }
 }
